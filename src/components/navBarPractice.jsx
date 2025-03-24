2ef440854126ff4012a8fea68efc1673f0a237fa
Marjon <marjonalarcon28@gmail.com>
2025-03-24 12:46:02 +0800

    implement react-route-dom

--- src/components/navBarPractice.jsx
+++ src/components/navBarPractice.jsx
@@ -1,34 +1,32 @@
-import './navBarPractice.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSmile } from '@fortawesome/free-solid-svg-icons'
-import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import './navBarPractice.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Calculator from '../pages/Calculator';
 
+const NavBarPractice = () => {
+  return (
+    <Router>
+      <div>
+        <nav className='navbar'>
+          <div className='nav-title'>
+            <FontAwesomeIcon icon={faHouse} />
+            <h4>My First Project</h4>
+          </div>
+          <div className='nav-links'>
+            <div><Link to='/'>Home</Link></div>
+            <div><Link to='/about'>About</Link></div>
+            <div><Link to='/services'>Services</Link></div>
+            <div><Link to='/contact'>Contact</Link></div>
+            <div><Link to='/calculator'>Calculator</Link></div>
+          </div>
+        </nav>
+        <Routes>
+          <Route path='/calculator' element={<Calculator />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-
-const navBarPractice  = () => {
- 
-    return (
-        <>
-     <nav className='navbar'>
-        <div className='nav-title'>
-      
-        <FontAwesomeIcon icon={faHouse} />  
-        <h4>My First Project</h4>
-    
-      
-            </div>
-        
-      <div className='nav-links'>
-            <div><a href='/'>Home</a></div>
-            <div><a href='/'>About</a></div>
-            <div><a href='/'>Services</a></div>
-            <div><a href='/'>Contact</a></div>
-        </div>
-     
-     </nav>
-     </>
-    )
-  }
-
-
-export default navBarPractice
+export default NavBarPractice;
